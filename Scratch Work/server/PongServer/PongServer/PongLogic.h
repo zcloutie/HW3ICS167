dd843687f1cdba4834fdc27e3a7b0bd7bf0e3c47
Harris Barber <captainotlir@gmail.com>
2018-02-14 14:42:26 -0800

    fixed server compile errors

--- Scratch Work/server/PongServer/PongServer/PongLogic.h
+++ Scratch Work/server/PongServer/PongServer/PongLogic.h
@@ -5,9 +5,97 @@ using namespace std;
 #define ARENA_WIDTH 600
 #define ARENA_HEIGHT 600
 
+class Paddle {
+public:
+	Paddle(int ClientID, int startX, int startY, int width, int height) : clientID(ClientID), x(startX), y(startY), width(width), height(height) {
+	}
+
+	void update() {
+		x += speed * ((-left)*(right ^ left)); // xor the values of right and left
+
+		if (x < 0) { //all the way to the left
+			x = 0;
+		}
+		else if (x + width > ARENA_WIDTH) { //all the way to the right
+			x = ARENA_WIDTH - width;
+		}
+	}
+
+	void setLeft(bool isDown) {
+		left = isDown;
+	}
+
+	void setRight(bool isDown) {
+		right = isDown;
+	}
+
+	int x;
+	int y;
+	int width = 50;
+	int height = 10;
+	const int speed = 4;
+	bool right = false;
+	bool left = false;
+	int score = 0;
+	int clientID;
+};
+
+class Ball {
+public:
+	Ball(int startX, int startY) : x(startX), y(startY) {
+	}
+
+	void update(vector<Paddle> paddles) {
+		x += xSpeed;
+		y += ySpeed;
+		int topX = x - radius;
+		int topY = y - radius;
+		int bottomX = x + radius;
+		int bottomY = y + radius;
+
+		if (topX < 0) {
+			x = radius;
+			xSpeed = -xSpeed;
+		}
+		else if (bottomX > ARENA_WIDTH) {
+			x = ARENA_WIDTH - radius;
+			xSpeed = -xSpeed;
+		}
+
+		if (y < 0) { //this, ofcourse, will need to be changed for Assignment 4
+			y = radius;
+			ySpeed = -ySpeed;
+		}
+
+		if (y > ARENA_HEIGHT) {
+			xSpeed = 0;
+			ySpeed = 3;
+			x = ARENA_WIDTH / 2;
+			y = ARENA_HEIGHT / 2;
+			paddles[0].score = 0; //this will have to be modified for Assignment 4 but should be fine for now
+		}
+
+		for (int i = 0; i < paddles.size(); i++) {
+			if (topY < (paddles[i].y + paddles[i].height) && bottomY > paddles[i].y && topX < (paddles[i].x + paddles[i].width) && bottomX > paddles[i].x) {
+				ySpeed = -3;
+				xSpeed += paddles[i].speed * ((-paddles[i].left)*(paddles[i].right ^ paddles[i].left)) / 2;
+				y += ySpeed;
+				paddles[i].score++;
+			}
+		}
+	}
+
+	int x;
+	int y;
+private:
+	const int radius = 5;
+	int xSpeed = 0;
+	int ySpeed = 3;
+};
+
 class GameState {
 public:
-	GameState() : ball(ARENA_WIDTH/2, ARENA_HEIGHT/2) {
+	GameState() : ball(ARENA_WIDTH / 2, ARENA_HEIGHT / 2), players{} {
 	}
 
 	bool isGameStarted() {
@@ -60,92 +148,4 @@ private:
 	vector<Paddle> players;
 	Ball ball;
 	bool gameStarted = false;
-};
-
-class Paddle {
-public:
-	Paddle(int ClientID, int startX, int startY, int width, int height) : clientID(ClientID), x(startX), y(startY), width(width), height(height) {
-	}
-
-	void update() {
-		x += speed * ((-left)*(right ^ left)); // xor the values of right and left
-
-		if (x < 0) { //all the way to the left
-			x = 0;
-		}
-		else if (x + width > ARENA_WIDTH) { //all the way to the right
-			x = ARENA_WIDTH - width;
-		}
-	}
-
-	void setLeft(bool isDown) {
-		left = isDown;
-	}
-
-	void setRight(bool isDown) {
-		right = isDown;
-	}
-
-	int x;
-	int y;
-    int width = 50;
-	int height = 10;
-	const int speed = 4;
-	bool right = false;
-	bool left = false;
-	int score = 0;
-	int clientID;
-};
-
-class Ball{
-public:
-	Ball(int startX, int startY) : x(startX), y(startY){
-	}
-
-	void update(vector<Paddle> paddles) {
-		x += xSpeed;
-		y += ySpeed;
-		int topX = x - radius;
-		int topY = y - radius;
-		int bottomX = x + radius;
-		int bottomY = y + radius;
-
-		if (topX < 0) {
-			x = radius;
-			xSpeed = -xSpeed;
-		}
-		else if (bottomX > ARENA_WIDTH) {
-			x = ARENA_WIDTH - radius;
-			xSpeed = -xSpeed;
-		}
-
-		if (y < 0) { //this, ofcourse, will need to be changed for Assignment 4
-			y = radius;
-			ySpeed = -ySpeed;
-		}
-
-		if (y > ARENA_HEIGHT) {
-			xSpeed = 0;
-			ySpeed = 3;
-			x = ARENA_WIDTH / 2;
-			y = ARENA_HEIGHT / 2;
-			paddles[0].score = 0; //this will have to be modified for Assignment 4 but should be fine for now
-		}
-
-		for (int i = 0; i < paddles.size(); i++) {
-			if (topY < (paddles[i].y + paddles[i].height) && bottomY > paddles[i].y && topX < (paddles[i].x + paddles[i].width) && bottomX > paddles[i].x) {
-				ySpeed = -3;
-				xSpeed += paddles[i].speed * ((-paddles[i].left)*(paddles[i].right ^ paddles[i].left)) / 2;
-				y += ySpeed;
-				paddles[i].score++;
-			}
-		}
-	}
-
-	int x;
-	int y;
-private:
-	const int radius = 5;
-	int xSpeed = 0;
-	int ySpeed = 3;
 };
