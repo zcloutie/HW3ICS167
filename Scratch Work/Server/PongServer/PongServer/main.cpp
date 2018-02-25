9e0596691ba0284db309fbc5f09fb1cef1e548ae
Harris Barber <captainotlir@gmail.com>
2018-02-25 15:55:42 -0800

    Started work on making the server have latency

--- Scratch Work/Server/PongServer/PongServer/main.cpp
+++ Scratch Work/Server/PongServer/PongServer/main.cpp
@@ -11,10 +11,73 @@ using namespace std;
 
 webSocket server;
 
+vector<pair<int, pair<string, int>>> inQueue{};
+vector<pair<int, pair<string, int>>> outQueue{};
+
+int latency = 0;
+
 int interval_clocks = CLOCKS_PER_SEC * INTERVAL_MS / 1000;
+
+void enqueInput(int clientID, string message, int expectedTime) {
+	inQueue.push_back(make_pair(expectedTime, make_pair(message, clientID)));
+}
+
+void processInput(int currentTime) {
+	for (int i = 0; i < inQueue.size(); i++) {
+		if (inQueue[i].first <= currentTime) {
+			pair<string, int> message = inQueue[i].second;
+
+			vector<int> clientIDs = server.getClientIDs();
+			if (message.first.substr(0, 2) == "ID") {
+				server.wssetClientCIDs(message.second, message.first.substr(3, message.first.size())); //client sent "ID:name"
+				string names = "";
+				for (int i = 0; i < clientIDs.size(); i++) {
+					names += "n" + to_string(i + 1) + ":" + server.getwsClientName(i) + "|";
+				}
+				for (int i = 0; i < clientIDs.size(); i++) {
+					server.wsSend(clientIDs[i], names);
+				}
+			}
+			else if (message.first.substr(0, 2) == "LD") { //When left button is pushed down
+				server.gameState.setClientLeft(message.second, true);
+			}
+
+			else if (message.first.substr(0, 2) == "LU") { //When left button is released
+				server.gameState.setClientLeft(message.second, false);
+			}
+
+			else if (message.first.substr(0, 2) == "RD") { //When right button is pushed down
+				server.gameState.setClientRight(message.second, true);
+			}
+
+			else if (message.first.substr(0, 2) == "RU") { //When left button is released
+				server.gameState.setClientRight(message.second, false);
+			}
+
+			inQueue.erase(find(inQueue.begin(), inQueue.end(), inQueue[i]));
+		}
+	}
+}
+
+void enqueOutput(int clientID, string message, int expectedTime) {
+	outQueue.push_back(make_pair(expectedTime, make_pair(message, clientID)));
+}
+
+void sendOutput(int currentTime) {
+	for (int i = 0; i < outQueue.size(); i++) {
+		if (outQueue[i].first <= currentTime) {
+			pair<string, int> message = outQueue[i].second;
+
+			server.wsSend(message.second, message.first);
+
+			outQueue.erase(find(outQueue.begin(), outQueue.end(), outQueue[i]));
+		}
+	}
+}
 
 /* called when a client connects */
 void openHandler(int clientID){
+	/*
     ostringstream os;
     os << "Stranger " << clientID << " has joined.";
 
@@ -23,11 +86,12 @@ void openHandler(int clientID){
         if (clientIDs[i] != clientID)
             server.wsSend(clientIDs[i], os.str());
     }
-    server.wsSend(clientID, "Welcome!");
+    server.wsSend(clientID, "Welcome!");*/
 }
 
 /* called when a client disconnects */
 void closeHandler(int clientID){
+	/*
     ostringstream os;
     os << "Stranger " << clientID << " has left.";
 
@@ -35,7 +99,7 @@ void closeHandler(int clientID){
     for (int i = 0; i < clientIDs.size(); i++){
         if (clientIDs[i] != clientID)
             server.wsSend(clientIDs[i], os.str());
-    }
+    }*/
 	server.gameState.stopGame();
 }
 
@@ -49,32 +113,7 @@ void messageHandler(int clientID, string message){
         if (clientIDs[i] != clientID)
             server.wsSend(clientIDs[i], os.str());
     }*/
-	vector<int> clientIDs = server.getClientIDs();
-	if (message.substr(0, 2) == "ID") { 
-		server.wssetClientCIDs(clientID, message.substr(3, message.size())); //client sent "ID:name"
-		string names = "";
-		for (int i = 0; i < clientIDs.size(); i++) {
-			names += "n" + to_string(i+1) + ":" + server.getwsClientName(i) + "|";
-		}
-		for (int i = 0; i < clientIDs.size(); i++) {
-			server.wsSend(clientIDs[i], names);
-		}
-	}
-	else if (message.substr(0, 2) == "LD") { //When left button is pushed down
-		server.gameState.setClientLeft(clientID, true);
-	}
-
-	else if (message.substr(0, 2) == "LU") { //When left button is released
-		server.gameState.setClientLeft(clientID, false);
-	}
-
-	else if (message.substr(0, 2) == "RD") { //When right button is pushed down
-		server.gameState.setClientRight(clientID, true);
-	}
-
-	else if (message.substr(0, 2) == "RU") { //When left button is released
-		server.gameState.setClientRight(clientID, false);
-	}
+	enqueInput(clientID, message, clock() + latency);
 }
 
 /* called once per select() loop */
@@ -82,25 +121,17 @@ void periodicHandler(){
 	static clock_t oldTime = clock();// +interval_clocks;
     clock_t newTime = clock();
     if (newTime >= oldTime + interval_clocks){
-		/*
-        ostringstream os;
-		//Deprecated ctime API in Windows 10
-		char timecstring[26];
-		ctime_s(timecstring, sizeof(timecstring), &current);
-		string timestring(timecstring);
-        timestring = timestring.substr(0, timestring.size() - 1);
-        os << timestring;
+		processInput(newTime);
 
-        vector<int> clientIDs = server.getClientIDs();
-        for (int i = 0; i < clientIDs.size(); i++)
-            server.wsSend(clientIDs[i], os.str());
-		*/
 		server.gameState.update(newTime - oldTime);
 
 		vector<int> clientIDs = server.getClientIDs();
 		for (int i = 0; i < clientIDs.size(); i++) {
 			server.wsSend(clientIDs[i], server.gameState.buildGameStateMessage());
+			enqueOutput(clientIDs[i], server.gameState.buildGameStateMessage(), newTime + latency);
 		}
+
+		sendOutput(newTime);
 
         oldTime = clock();
     }
